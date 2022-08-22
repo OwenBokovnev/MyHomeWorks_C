c84490d3cfd3a9fb49ee27c444df590ab2539bd3
OwenBokovnev <ebokovnev@gmail.com>
2022-08-22 13:11:07 +0300

    HomeWork Seminar02 added

--- Program.cs
+++ Program.cs
@@ -1,9 +1,5 @@
 ﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 /*
-a = 5; b = 7 -> max = 7
-a = 2 b = 10 -> max = 10
-a = -9 b = -3 -> max = -3
-
 Console.WriteLine("Enter 1st number");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter 2st number");
@@ -26,51 +22,53 @@ else
 */
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 /*
-2, 3, 7 -> 7
-44 5 78 -> 78
-22 3 9 -> 22
-*/
 Console.WriteLine("Enter THREE numbers:");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 int number3 = Convert.ToInt32(Console.ReadLine());
-int numberMax = 0;
 
 if(number1 > number2 & number1 > number3)
 {
-    numberMax = number1;
-    Console.WriteLine($"{numberMax} is maximum number");
+    Console.WriteLine($"{number1} is maximum number");
 }
 if(number2 > number3 & number2 > number1)
 {
-    numberMax = number2;
-    Console.WriteLine($"{numberMax} is maximum number");
+    Console.WriteLine($"{number2} is maximum number");
 }
 if(number3 > number1 & number3 > number2)
 {
-    numberMax = number3;
-    Console.WriteLine($"{numberMax} is maximum number");
+    Console.WriteLine($"{number3} is maximum number");
 }
 else
 {
-    Console.WriteLine("You entered two or more equal numbers. Reenter different numbers, please!");
+    Console.WriteLine("You entered two or more EQUAL numbers. Please, reenter different numbers!");
 }
-// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-/*
-4 -> да
--3 -> нет
-7 -> нет
 */
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 /*
 Console.WriteLine("Enter number:");
 int numberA = Convert.ToInt32(Console.ReadLine());
 
 if(numberA % 2 == 0)
 {
-    Console.WriteLine($"Entering number {numberA} is EVEN");
+    Console.WriteLine($"Entered number {numberA} is EVEN");
 }
 else
 {
-    Console.WriteLine($"Entering number {numberA} is ODD");
+    Console.WriteLine($"Entered number {numberA} is ODD");
 }
 */
+// Задача 8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Enter number:");
+int number = Convert.ToInt32(Console.ReadLine());
+int counter = 1;
+
+while(counter <= number)
+{
+    if(counter % 2 == 0)
+    {
+        Console.WriteLine(counter + " ");
+    }
+counter ++;
+}
